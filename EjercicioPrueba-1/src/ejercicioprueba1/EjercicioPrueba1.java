5b4db25154bb6ebf182185ca0af165013a1f1788
Snt1ago <99658173+Snt1ago@users.noreply.github.com>
2022-06-11 20:21:27 -0300

    Add files via upload

---
+++ EjercicioPrueba-1/src/ejercicioprueba1/EjercicioPrueba1.java
@@ -0,0 +1,14 @@
+package ejercicioprueba1;
+
+public class EjercicioPrueba1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub		
+		System.out.println("Hola profe mi nombre es Santiago Icasuriaga");
+
+		  
+		
+
+	}
+
+}
---
+++ EjercicioPrueba-1/src/module-info.java
@@ -0,0 +1,2 @@
+module EjercicioPrueba1 {
+}
